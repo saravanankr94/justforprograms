fa8b69192d9603e65f2caaddcdc118813d3dae17
saravanankr94 <saravanankr94@gmail.com>
2017-06-16 19:17:30 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+class AddNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+      x=1;
+      y=2;
+      z = x + y;
+      System.out.println("Sum of entered integers = "+z);
+   }
+}
